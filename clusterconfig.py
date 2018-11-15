903ee31052222c331335e840fb72df95ae2b796f
Keith <hnefatl@gmail.com>
2018-11-15 17:03:36 +0000

    Added config source file...

---
+++ clusterconfig.py
@@ -0,0 +1,23 @@
+import json
+
+class ClusterConfig:
+    def __init__(self):
+        # Default values
+        self.zone = "eu-west-1a"
+        self.master_type = "c4.large"
+        self.slave_type = "t2.small"
+        self.init_master_count = 1
+        self.init_slave_count = 10
+        self.cluster_name = "group8.cluster.k8s.local"
+        self.s3_bucket_prefix = "kubernetes.group8"
+
+    def json_show(self):
+        return json.dumps(self.__dict__, indent=4)
+
+    def json_load_path(self, json_path):
+        with open(json_path, mode="r+") as file:
+            contents = file.read()
+        self.json_load(contents)
+
+    def json_load(self, json_text):
+        self.__dict__ = json.loads(json_text)
