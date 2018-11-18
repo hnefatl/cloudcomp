dc04703f0a3f5ae31be7fd453e6c3696969aa440
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-11-18 20:24:17 +0000

    Change s3n to s3a

--- wordlettercount/test.py
+++ wordlettercount/test.py
@@ -5,12 +5,13 @@ import re
 
 sc = pyspark.SparkContext("local", "WordLetterCount", pyFiles=[])
 
-text_file = sc.textFile("s3n://group8.foo/asdf")
+text_file = sc.textFile("s3a://group8.foo/asdf")
 
-results = text_file \
-            .flatMap(lambda line: re.findall(r"\w+", line)) \
-            .map(lambda word: (word, 1)) \
-            .reduceByKey(lambda x, y: x + y) \
-            .sortBy(lambda x: x[1], False)
+results = (
+    text_file.flatMap(lambda line: re.findall(r"\w+", line))
+    .map(lambda word: (word, 1))
+    .reduceByKey(lambda x, y: x + y)
+    .sortBy(lambda x: x[1], False)
+)
 
 results.saveAsTextFile("output.txt")
