4b7e2dc73d29c11b62e93b9793605e529dbb187c
Keith <hnefatl@gmail.com>
2018-11-19 14:20:59 +0000

    Fleshed out the processing stage, changed to take keys from command line

--- wordlettercount/test.py
+++ wordlettercount/test.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 import pathlib
 import pyspark
 import re
 
+if len(sys.argv) != 3:
+    raise RuntimeError("Usage: test.py <access_key> <secret_key>")
+
+access_key = sys.argv[1]
+secret_key = sys.argv[2]
+
+# TODO(kc506): Move into CLI, pass keys to spark-submit
 def load_creds():
     path = pathlib.Path.home() / pathlib.Path(".aws/credentials")
     with path.open() as f:
@@ -20,7 +28,6 @@ def load_creds():
             secret_key = segs[1]
     return (access_key, secret_key)
 
-access_key, secret_key = load_creds()
 print(f"Access Key: {access_key}, Secret Key: {secret_key}")
 
 sc = pyspark.SparkContext("local", "WordLetterCount", pyFiles=[])
@@ -29,11 +36,22 @@ sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", secret_key)
 
 text_file = sc.textFile("s3a://group8.foo/asdf")
 
+def mapper(word):
+    return [((word, "w"), 1)] + [((letter, "l"), 1) for letter in word]
+
 results = (
     text_file.flatMap(lambda line: re.findall(r"\w+", line))
-    .map(lambda word: (word, 1))
-    .reduceByKey(lambda x, y: x + y)
-    .sortBy(lambda x: x[1], False)
+    .flatMap(mapper) # Add tags for words/letters along with initial counts
+    .reduceByKey(lambda x, y: x + y) # Sum frequencies of each word/letter
+    .sortBy(lambda x: x[1], False) # Sort by frequency
+    .groupBy(lambda x: x[0][1]) # Group into words vs letters (sorted order is preserved)
+    .mapValues(lambda x: map(lambda y: (y[0][0], y[1]), x)) # Remove the tag from the value, as we're now grouped by tag
+    .mapValues(list) # Convert the results of groupBy from iterables to lists
+    .collectAsMap() # Get words
 )
 
-results.saveAsTextFile("output.txt")
+def process():
+    pass
+
+print(results)
+#results.saveAsTextFile("output.txt")
