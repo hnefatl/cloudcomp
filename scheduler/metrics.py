7899bc2346af309e37ad6581c3ab80f3d38b0445
Keith <hnefatl@gmail.com>
2018-11-27 11:37:16 +0000

    Added various metrics functions

---
+++ scheduler/metrics.py
@@ -0,0 +1,104 @@
+import kubernetes
+import subprocess
+import json
+import yaml
+import urllib
+
+
+# Deploy the metrics server pods onto the cluster
+def deploy_metrics():
+    # We need to spin up metrics-server pods
+    url = "https://raw.githubusercontent.com/kubernetes-incubator/metrics-server/master/deploy/1.8+"
+    # Process creation arguments - we append a url to the end
+    args = ["kubectl", "create", "-f"]
+    # Most of the config files we can use directly
+    files = [
+        "aggregated-metrics-reader.yaml",
+        "auth-delegator.yaml",
+        "auth-reader.yaml",
+        "metrics-apiservice.yaml",
+        "metrics-server-service.yaml",
+        "resource-reader.yaml",
+    ]
+    for file in files:
+        subprocess.check_call(args + [f"{url}/{file}"])
+
+    # Edit the main deployment file to allow insecure traffic
+    file = "metrics-server-deployment.yaml"
+    with urllib.request.urlopen(f"{url}/{file}") as response:
+        docs = list(yaml.load_all(response))
+    for doc in docs:
+        try:
+            for container in doc["spec"]["template"]["spec"]["containers"]:
+                if container["name"] == "metrics-server":
+                    container["args"] = ["--kubelet-insecure-tls"]
+        except KeyError:
+            pass
+
+    # Pipe the altered yaml into kubectl create
+    with subprocess.Popen(args + ["-"], stdin=subprocess.PIPE) as p:
+        yaml.dump_all(docs, stream=p.stdin, encoding="utf-8")
+
+
+def undeploy_metrics():
+    url = "https://raw.githubusercontent.com/kubernetes-incubator/metrics-server/master/deploy/1.8+"
+    files = [
+        "aggregated-metrics-reader.yaml",
+        "auth-delegator.yaml",
+        "auth-reader.yaml",
+        "metrics-apiservice.yaml",
+        "metrics-server-service.yaml",
+        "metrics-server-deployment.yaml",
+        "resource-reader.yaml",
+    ]
+    for file in files:
+        subprocess.check_call(["kubectl", "delete", "-f", f"{url}/{file}"])
+
+
+# Returns a dictionary mapping pod names to dictionaries containing the window in seconds of the metrics, the
+# timestamp of the measurement the cpu
+# usage percentage (0 being 0%, 1 being 100%), and the memory usage in KiB.
+def get_pod_metrics():
+    output = subprocess.check_output(
+        ["kubectl", "get", "--raw", "/apis/metrics.k8s.io/v1beta1/pods"]
+    )
+    metrics = {}
+    for usage_item in json.loads(output)["items"]:
+        try:
+            if usage_item["metadata"]["namespace"] == "kube-system":
+                continue
+            containers = usage_item["containers"]
+            cpu = sum(
+                convert_cpu(container["usage"]["cpu"]) for container in containers
+            )
+            mem = sum(
+                convert_mem(container["usage"]["memory"]) for container in containers
+            )
+            metrics[usage_item["metadata"]["name"]] = {
+                "window": int(usage_item["window"][:-1]),
+                "cpu": cpu,
+                "mem": mem,
+            }
+        except KeyError:
+            pass  # Malformed response, ignore
+    return metrics
+
+
+def convert_cpu(cpu):
+    if cpu.endswith("n"):  # Nanoseconds...
+        return int(cpu[:-1]) / 10 ** 9
+    else:
+        return int(cpu)
+
+
+def convert_mem(mem_usage):
+    if mem_usage.endswith("Ki"):
+        return int(mem_usage[:-2])
+    elif mem_usage.endswith("Mi"):
+        return int(mem_usage[:-2]) * 1024
+    elif mem_usage.endswith("K"):
+        return int(float(mem_usage[:-1]) * 1.024)
+    elif mem_usage.endswith("M"):
+        return int(float(mem_usage[:-1]) * 1.000_024)
+    else:
+        return int(mem_usage)
